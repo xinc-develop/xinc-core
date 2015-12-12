944a28e0726e5582fd55c23a30ef9681476754e0
Sebastian B. Knapp <rock@ccls-online.de>
2015-12-12 14:31:26 +0100

    add test to Singleton class (2)

---
+++ Classes/Test/Singleton.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Xinc - Continuous Integration.
+ *
+ * @author    Arno Schneider
+ * @author    Alexander Opitz
+ * @author    Sebastian Knapp
+ * @copyright 2014 Alexander Opitz, Leipzig
+ * @copyright 2015 Xinc Development Team, https://github.com/xinc-develop/
+ * @license   http://www.gnu.org/copyleft/lgpl.html GNU/LGPL, see license.php
+ *            This file is part of Xinc.
+ *            Xinc is free software; you can redistribute it and/or modify
+ *            it under the terms of the GNU Lesser General Public License as
+ *            published by the Free Software Foundation; either version 2.1 of
+ *            the License, or (at your option) any later version.
+ *
+ *            Xinc is distributed in the hope that it will be useful,
+ *            but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *            GNU Lesser General Public License for more details.
+ *
+ *            You should have received a copy of the GNU Lesser General Public
+ *            License along with Xinc, write to the Free Software Foundation,
+ *            Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @homepage  https://github.com/xinc-develop/xinc-core/
+ */
+
+/** 
+ * A Singleton Pattern Implementation 
+ */
+namespace Xinc\Core\Test;
+
+use Xinc\Core\Singleton as Base;
+/**
+ * Base class for classes which needs to have a singleton instance.
+ */
+class Singleton extends Base
+{
+
+}
