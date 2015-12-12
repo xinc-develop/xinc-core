e4392db6c22f5c8a4a6486ab6a6a48e4474b0346
Sebastian B. Knapp <rock@ccls-online.de>
2015-12-12 14:00:40 +0100

    php-cs-fixer use

--- help.sh
+++ help.sh
@@ -16,6 +16,17 @@ perl -pi -e 's/http:\/\/code\.google\.com\/p\/xinc\//https:\/\/github.com\/xinc-
 perl -pi -e 's/http:\/\/xincplus\.sourceforge\.net/https:\/\/github.com\/xinc-develop\/xinc-core\//g' \
     $(find Classes -type f)
 
+if [ -L php-cs-fixer ] ; then true ; else
+    if [ -f vendor/bin/php-cs-fixer ] ; then true ; else
+        mkdir -p vendor/bin
+        curl http://get.sensiolabs.org/php-cs-fixer.phar -o vendor/bin/php-cs-fixer
+        chmod +x vendor/bin/php-cs-fixer
+    fi
+    ln -s vendor/bin/php-cs-fixer php-cs-fixer
+fi
+
+#find Classes -name '*.php' -exec ./php-cs-fixer fix '{}' \;
+#./php-cs-fixer fix 
 
 rm -rf docs/*
 doxygen
